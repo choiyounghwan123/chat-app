4fa927ca91da8c3415c4462e3f97529d78838542
choiyounghwan <egoing@gmail.com>
2022-11-10 14:11:54 +0900

    socketconnet

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require('express')();
 const server = require('http').createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', socket =>{
+    console.log('a user connected');
+});
 
 server.listen(8080,()=>{
     console.log('Server is running on 8080 port');
