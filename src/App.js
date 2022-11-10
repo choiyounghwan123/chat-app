670674d3f3d33eb4308d4dd9467b9910460a7fe3
choiyounghwan <egoing@gmail.com>
2022-11-10 14:40:17 +0900

    ver4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {io} from "socket.io-client";
+import { useState } from 'react';
 function App() {
 
   let socket = io("http://localhost:8080",{
@@ -8,9 +9,23 @@ function App() {
     }
   });
 
+  let [msg,setMsg] = useState('');
+
+  const onChangeInput = (e) =>{
+    setMsg(e.target.value);
+    console.log(msg);
+  }
+
+
   return (
     <div className="App">
      
+    <input type="text" value={msg} onChange={onChangeInput}></input>
+    <button onClick={()=>{
+      socket.emit('msg',msg);
+      setMsg('');
+    }}>emit</button>
+
     </div>
   );
 }
